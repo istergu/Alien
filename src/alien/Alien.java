c41d02441a3a5108fd94a69e8e5ec6eeaa75151c
IllyrianG0T <49834648+illyriangott@users.noreply.github.com>
2019-04-21 15:49:32 +0200

    * Nderrova authorin

--- src/alien/Alien.java
+++ src/alien/Alien.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package alien;
 
 import javafx.application.Application;
@@ -15,7 +11,7 @@ import javafx.stage.Stage;
 
 /**
  *
- * @author Geni
+ * @author Nacut
  */
 public class Alien extends Application {
     
