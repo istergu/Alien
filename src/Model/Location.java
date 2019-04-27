6b6c2c474674a161c92ae305838be6fa92e5380c
alliuqemal <36800086@users.noreply.github.com>
2019-04-27 10:04:31 +0200

    AL - 1
    Location class, getters & setters

---
+++ src/Model/Location.java
@@ -0,0 +1,42 @@
+package Model;
+
+public class Location {
+    private String name;
+    private float x;
+    private float y;
+    private float distanceFromTR;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getDistanceFromTR() {
+        return distanceFromTR;
+    }
+
+    public void setDistanceFromTR(float distanceFromTR) {
+        this.distanceFromTR = distanceFromTR;
+    }
+    
+    
+}
